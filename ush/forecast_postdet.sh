e555392afa0811aa4a3fe1c24f1b240be0192e59
tsga <zacctsega@yahoo.com>
2025-01-03 12:54:50 +0000

    turn off land iau for gfs

--- ush/forecast_postdet.sh
+++ ush/forecast_postdet.sh
@@ -137,6 +137,7 @@ FV3_postdet() {
         IAU_INC_FILES="''"
       fi
       if [[ ${DO_LAND_IAU} = "YES" ]]; then
+	DO_LAND_IAU="NO"
         LAND_IAU_FHRS=-1
         LAND_IAU_DELTHRS=0
         LAND_IAU_INC_FILES="''"
@@ -215,7 +216,7 @@ EOF
           fi
           ${NCP} "${sfc_increment_file}" "${DATA}/INPUT/sfc_inc.tile${TN}.nc"
         done
-	#LAND_IAU_INC_FILES="${LAND_IAU_INC_FILES:-'sfc_inc'}"
+	LAND_IAU_INC_FILES=${LAND_IAU_INC_FILES:-'sfc_inc'}
 
       #else #TODO: check to make sure correct sfc inc for non-IAU
       fi
